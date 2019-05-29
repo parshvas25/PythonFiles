84be4b9699f3176e981b68ed860e88a2fbf01d75
Parshva Shah <parshvashah414@gmail.com>
2019-05-29 01:25:08 -0400

    Add files via upload

---
+++ pygame.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("First Game")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while run:
+    pygame.time.delay(100)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
